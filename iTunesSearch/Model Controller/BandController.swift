7dfb93e83fafe3d9500f9aad61c5a794ebec84d5
reich452 <reich452@regis.edu>
2017-11-01 15:40:47 -0600

    Added fetch function

--- iTunesSearch/Model Controller/BandController.swift
+++ iTunesSearch/Model Controller/BandController.swift
@@ -13,14 +13,41 @@ class BandController {
     
     let baseUrl = URL(string: "https://itunes.apple.com/search?")!
     
-    func fetchBand(matching artistName: String, completion: @escaping ([Band]) -> Void) {
+    func fetchBand(matching searchTerm: String, completion: @escaping ([Band]?) -> Void) {
+        let url = bandURL(for: searchTerm)
         
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            
+            do {
+                let jsonDecoder = JSONDecoder()
+                
+                if let error = error {
+                    print("Error getting back json \(error.localizedDescription) \(#file) \(#function)")
+                    completion([])
+                    return
+                }
+                guard let data = data else {
+                    print("Problem getting data")
+                    completion([]); return }
+                
+                let bands = try jsonDecoder.decode(Bands.self, from: data)
+                completion(bands.results)
+                
+            } catch let error {
+                print("Either no data was returned, or data was not serialized \(error.localizedDescription)")
+                completion([])
+                return
+            }
+            
+            }.resume()
     }
+    
+    // TODO: - Fetch Image 
     
     
     
     private func bandURL(for artistName: String) -> URL {
-
+        
         var components = URLComponents(url: baseUrl, resolvingAgainstBaseURL: true)!
         let queryItems = [URLQueryItem(name: "term", value: artistName)]
         components.queryItems = queryItems
