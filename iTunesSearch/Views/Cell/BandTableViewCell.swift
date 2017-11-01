53bd05b9c115bd4bd8aa2a8073bf89a8ae5285c6
reich452 <reich452@regis.edu>
2017-11-01 15:44:32 -0600

    Added TVC and customCell

---
+++ iTunesSearch/Views/Cell/BandTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BandTableViewCell.swift
+//  iTunesSearch
+//
+//  Created by Nick Reichard on 11/1/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+import UIKit
+
+class BandTableViewCell: UITableViewCell {
+
+
+
+}
